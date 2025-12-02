002efce5a413dd82f54eaff02e026c8a5dd681a3
Boburov Shukurullo <boburovdev@gmail.com>
2025-12-02 20:47:36 +0500

    fixed error

--- app/(pages)/verify/page.tsx
+++ app/(pages)/verify/page.tsx
@@ -1,32 +1,7 @@
-"use client";
-
-import authService from "@/app/api/service/auth.service";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
 const page = () => {
-  const token = useSearchParams().get("token");
-  const [message, setMessage] = useState("");
-  const router = useRouter();
+  return (
+    <div>page</div>
+  )
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await authService.verifyToken(String(token));
-
-      if (res.status === 201) {
-        localStorage.setItem("admin_mail", res.data.email);
-        setMessage("token qabul qilindi");
-      } else {
-        console.log("token noto'gri");
-        setMessage("token noto'gri");
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return <div>{message}</div>;
-};
-
-export default page;
+export default page
